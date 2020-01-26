f2ddffb80d66407e7ed20bc0da4c6cf9055f8f39
yourUsername <yourEmail@domain.com>
2020-01-26 16:43:05 -0500

    gg

---
+++ src/test/java/com/example/demo/hello.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class hello {
+public static void main(String[]args) {
+
+int c = 43;
+double v = 22;
+
+  double sum = c+v;
+    System.out.println("sum: "+ sum);
+  double mul = c*v;
+    System.out.println("mul: "+ mul);
+  double avg = c/v;
+    System.out.println("avg: "+ avg);
+  double min = c-v;
+     System.out.println("min: "+ min);
+}
+
+
+}
